f416fcc097ade5758a8a6bad3bea208e8df1fa3f
Tirilance <tirilance@gmail.com>
2022-04-20 22:55:08 -0500

    Updated APIVersion.

--- GrindTimer.lua
+++ GrindTimer.lua
@@ -1,7 +1,7 @@
 GrindTimer = {}
 
 GrindTimer.Name = "GrindTimer"
-GrindTimer.Version = "1.13.5"
+GrindTimer.Version = "1.13.6"
 GrindTimer.SavedVariableVersion = "6"
 GrindTimer.AccountSavedVariablesVersion = "6"
 GrindTimer.UIInitialized = false
