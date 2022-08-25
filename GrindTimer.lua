62dda527f0a83b9ed9185a696c8276988170b6b3
Tirilance <tirilance@gmail.com>
2022-08-25 07:44:33 -0500

    Updated addon version to 1.13.7

--- GrindTimer.lua
+++ GrindTimer.lua
@@ -1,7 +1,7 @@
 GrindTimer = {}
 
 GrindTimer.Name = "GrindTimer"
-GrindTimer.Version = "1.13.6"
+GrindTimer.Version = "1.13.7"
 GrindTimer.SavedVariableVersion = "6"
 GrindTimer.AccountSavedVariablesVersion = "6"
 GrindTimer.UIInitialized = false
